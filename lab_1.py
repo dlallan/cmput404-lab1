4eec2d8ba64b33d6a7514acfa4231dd1f7daa012
Dillon Allan <dallan@ualberta.ca>
2022-01-10 19:25:58 -0700

    Add quine step and some housekeeping

--- lab_1.py
+++ lab_1.py
@@ -1,8 +1,17 @@
+import os
 import requests
 
-# virtualenv Step 4
-print(f"requests version: {requests.__version__}")
+SECTION_SEPARATOR = os.linesep*2
 
-# curl step 5
+# virtualenv Step 4
+print(f"requests version: {requests.__version__}", end=SECTION_SEPARATOR)
+
+# curl Step 5
 response = requests.get('http://www.google.com/')
-print(f"Headers: {response.headers}", f"Content: {response.content}", sep='\n\n')
+print(f"Headers: {response.headers}",
+      f"Content: {response.content}", sep=SECTION_SEPARATOR, end=SECTION_SEPARATOR)
+
+# curl Step 10
+with requests.get('https://raw.githubusercontent.com/dlallan/cmput404-lab1/main/lab_1.py') as response:
+    quine_text = response.content.decode()
+    print("Script contents:", quine_text, sep=os.linesep)
